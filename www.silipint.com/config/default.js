cd08205f773ea0d389e7bf42dfe0012a9938622c
Jason Sebring <mail@jasonsebring.com>
2013-12-02 21:16:03 -0800

    initial

---
+++ www.silipint.com/config/default.js
@@ -0,0 +1,16 @@
+var kitguiAccountKey = '[ FILL IN ]';
+module.exports = {
+	kitgui : {
+		host : 's3.amazonaws.com',
+		basePath : '/kitgui/clients/' + kitguiAccountKey,
+		accountKey : kitguiAccountKey
+	},
+	hubsoft : {
+		clientid : 'silipint'
+	},
+	mailchimp : {
+		apikey : '[ FILL IN ]-us3',
+		url : 'https://us3.api.mailchimp.com/2.0/',
+		listID : '[ FILL IN ]'
+	}
+};
