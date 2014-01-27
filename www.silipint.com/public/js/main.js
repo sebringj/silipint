5d6ccd745369b40d97eefc8d6d42d9f6b52d3105
Jason Sebring <mail@jasonsebring.com>
2014-01-27 09:59:31 -0800

    updates

--- www.silipint.com/public/js/main.js
+++ www.silipint.com/public/js/main.js
@@ -1,24 +1,33 @@
 // enable popout menu
 $(window).resize(function(){
+	var iof = 'iosorientationfix';
 	$('#mobileMenu .nav').css({
 		height: 10000
 	});
-	if ($(window).data('iosorientationfix')) {
-		clearInterval($(window).data('iosorientationfix'));
+	if ($(window).data(iof)) {
+		clearInterval($(window).data(iof));
 	}
-	$(window).data('iosorientationfix', setTimeout(function(){
+	$(window).data(iof, setTimeout(function(){
 		var h = $(window).height();
 		$('#mobileMenu .nav').css({
 			height: h + 20
 		});
 	},10));
 });
-$('#header .more-sili, #toggleMobileMenu, #pushUpMenu .close, #mobileMenu .close').click(function(ev){
-	ev.preventDefault();
-	console.log('toggle');
-	if ($('body').hasClass('menuOpen')) {
-		$('body').removeClass('menuOpen');
-	} else {
-		$('body').addClass('menuOpen');
+// handle menu toggle as quick as possible for mobile
+(function(){ 
+	function handleToggle(ev) {
+		ev.preventDefault();
+		if ($('body').hasClass('menuOpen')) {
+			$('body').removeClass('menuOpen');
+		} else {
+			$('body').addClass('menuOpen');
+		}
 	}
-});
+	var $els = $('#header .more-sili, #toggleMobileMenu, #pushUpMenu .close, #mobileMenu .close');
+	if ('ontouchstart' in document.documentElement) {
+		$els.on('touchstart', handleToggle);
+	} else {
+		$els.on('click', handleToggle);
+	}
+})();
